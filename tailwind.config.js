f0ba9888eeccebb8a1702078b57282502c26f585
Edward Arechwa <e.arechwa@gmail.com>
2022-04-10 20:13:45 +0200

    initial structure

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{html,js,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        base: ["Roboto"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
